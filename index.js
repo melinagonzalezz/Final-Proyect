02fb9363760260a9dec6ead789920cc8c920e025
Dana959 <danaamachado@gmail.com>
2024-08-28 10:09:39 -0300

    Add files via upload
    Actualizado Index.html e index.js con desafíate entrega 2.

---
+++ index.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function(){
+    document.getElementById("autos").addEventListener("click", function() {
+        localStorage.setItem("catID", 101);
+        window.location = "products.html"
+    });
+    document.getElementById("juguetes").addEventListener("click", function() {
+        localStorage.setItem("catID", 102);
+        window.location = "products.html"
+    });
+    document.getElementById("muebles").addEventListener("click", function() {
+        localStorage.setItem("catID", 103);
+        window.location = "products.html"
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const username = localStorage.getItem('username');
+    
+    // Si no hay usuario autenticado, redirigir a login.html
+    if (!username) {
+        window.location.href = 'login.html';
+        return;
+    }
+    
+    //Mostrar nombre de usuario al inicial sesión
+    const usernameDisplay = document.getElementById ('username-display');
+    usernameDisplay.textContent = username;
+
+    // Manejo del botón de cerrar sesión
+    const logoutButton = document.getElementById('logoutButton');
+    logoutButton.addEventListener('click', function () {
+        localStorage.removeItem('username');
+        window.location.href = 'login.html';
+    });
+});
